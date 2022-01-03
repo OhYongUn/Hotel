4f6e837c6ae6d92a06d96d374832341bd04528a3
dhdyd <dhdyd@DESKTOP-TOBDRO2>
2022-01-03 21:17:45 +0900

    hi

---
+++ src/main/java/com/woori/hotel/WooriHotelApplication.java
@@ -0,0 +1,13 @@
+package com.woori.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WooriHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WooriHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woori/hotel/WooriHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woori.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WooriHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
